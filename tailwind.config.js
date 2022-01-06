42a7526d0d8a920a026e6354f5b25d6d728049c4
Pedro Morim <1180798@isep.ipp.pt>
2022-01-06 01:33:56 +0000

    feat: :lipstick: added Uphold's color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        uphold: '#49cc68',
+      },
+    },
   },
   plugins: [],
 };
