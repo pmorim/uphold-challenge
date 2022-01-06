84dfed65082f83ec9312cd48d4354effdc535c0d
Pedro Morim <1180798@isep.ipp.pt>
2022-01-06 20:06:35 +0000

    feat(hook): :sparkles: added debounce mechanism to exchange rates

--- src/hooks/useExchangeRates/useExchangeRates.ts
+++ src/hooks/useExchangeRates/useExchangeRates.ts
@@ -1,5 +1,6 @@
 import { useCallback, useEffect } from 'react';
 import { useLocalStorage } from 'usehooks-ts';
+import lodash from 'lodash';
 
 import { useSDK } from '..';
 import { getCurrencyFromPair } from '../../utils';
@@ -18,31 +19,34 @@ export interface ExchangeRate {
   currency: string;
 }
 
-export function useExchangeRates(currency: string, n = 10) {
+export function useExchangeRates(currency: string, n = 10, debounceWait = 100) {
   const sdk = useSDK();
 
   // Make sure that the rates are cached to the local storage
   const [rates, setRates] = useLocalStorage<ExchangeRate[]>('rates', []);
 
-  const updateRates = useCallback(async (currency: string) => {
-    // Grab the 10 first currency pairs
-    let pairs: CurrencyPair[] = (await sdk.getTicker(currency)).slice(0, n);
+  const updateRates = useCallback(
+    lodash.debounce(async () => {
+      // Grab the 10 first currency pairs
+      let pairs: CurrencyPair[] = (await sdk.getTicker(currency)).slice(0, n);
 
-    // Convert them to a simpler structure
-    setRates(
-      pairs.map(
-        ({ bid, currency, pair }): ExchangeRate => ({
-          rate: bid, // Should it use 'bid' or 'ask'?
-          currency: getCurrencyFromPair(pair, currency),
-        }),
-      ),
-    );
-  }, []);
+      // Convert them to a simpler structure
+      setRates(
+        pairs.map(
+          ({ bid, currency, pair }): ExchangeRate => ({
+            rate: bid, // Should it use 'bid' or 'ask'?
+            currency: getCurrencyFromPair(pair, currency),
+          }),
+        ),
+      );
+    }, debounceWait),
+    [currency, n, debounceWait],
+  );
 
   // Fetch the new rates on load and when currency is updated
   useEffect(() => {
-    updateRates(currency);
-  }, [currency]);
+    updateRates();
+  }, [updateRates]);
 
   return [rates, updateRates];
 }
