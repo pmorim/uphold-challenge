9fcee623dcfb5bdbec71bfc92dde22f503793d43
Pedro Morim <1180798@isep.ipp.pt>
2022-01-06 00:15:19 +0000

    chore: Added code styling and linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div>
-      <h1 className="pl-10 text-red-500">Hello World</h1>
+      <h1 className='pl-10 text-red-500'>Hello World</h1>
     </div>
   );
 }
