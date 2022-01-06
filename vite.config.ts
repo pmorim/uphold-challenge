f9aca337d6a986f434ac3c7bbb878f552e89bc78
Pedro Morim <1180798@isep.ipp.pt>
2022-01-06 22:29:38 +0000

    chore: :construction_worker: fixed SVG import problem

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactSvgPlugin from 'vite-plugin-react-svg';
+//import reactSvgPlugin from 'vite-plugin-react-svg';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactSvgPlugin()],
+  plugins: [react(), svgrPlugin()],
 });
