93c118ec9ba5b9ff1cb7e53bc07a4fa32360e4f2
Pedro Morim <1180798@isep.ipp.pt>
2022-01-07 16:02:59 +0000

    chore: :heavy_plus_sign: added tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  plugins: [require('tailwind-scrollbar'), require('@tailwindcss/forms')],
   theme: {
     extend: {
       colors: {
@@ -13,5 +14,4 @@ module.exports = {
       },
     },
   },
-  plugins: [],
 };
