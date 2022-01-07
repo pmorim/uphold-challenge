bbec772c18d57afacea475f68e3ac33ecac49242
Pedro Morim <1180798@isep.ipp.pt>
2022-01-07 22:50:16 +0000

    chore: :memo: created simple workaround for SDK types

--- src/@types/uphold-sdk-javascript.d.ts
+++ src/@types/uphold-sdk-javascript.d.ts
@@ -1,4 +1,9 @@
+/**
+ * This is just a simple workaround for the lack of types on the SDK module
+ */
+
 declare module '@uphold/uphold-sdk-javascript' {
-  const foo: unknown;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const foo: any;
   export = foo;
 }
