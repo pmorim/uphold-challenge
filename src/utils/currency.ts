f5104d8fd55423e60b98eae48da83b9a5b02f593
Pedro Morim <1180798@isep.ipp.pt>
2022-01-07 22:51:02 +0000

    chore: :memo: added simple utility documentation

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,22 +1,27 @@
-export function getCurrencyKeyFromPair(pair: string, base: string) {
+// Remove the baseCurrency from the pair to retrieve the wanted currency
+export function getCurrencyCodeFromPair(pair: string, base: string) {
   return pair.replace(base, ''); // Example: "EURUSD" -> "EUR"
 }
 
+// Force the number to have a maximum amount of decimal places
 export function limitDecimalPlaces(number: number | string, decimalPlaces = 6) {
   if (typeof number === 'string')
     number = Number.parseFloat(number.replace(',', ''));
   return parseFloat(number.toFixed(decimalPlaces)).toString();
 }
 
+// Add commas every 3 digits before the decimal point
 export function formatCurrency(amount: number | string) {
   if (typeof amount === 'number') amount = amount.toString();
   return amount.replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
+// Apply the conversion rate to the desired amount
 export function applyRate(rate: string, amount: string) {
   return (Number.parseFloat(rate) * Number.parseFloat(amount)).toString();
 }
 
+// Parse the user's "currency amount" input
 export function parseInput(
   input: string,
   setValue: (newValue: string) => void,
