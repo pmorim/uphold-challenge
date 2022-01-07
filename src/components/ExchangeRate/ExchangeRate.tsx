349f6c53900b4a12f3c70f7f3ff991a73f72c39b
Pedro Morim <1180798@isep.ipp.pt>
2022-01-07 16:40:36 +0000

    fix: :lipstick: fixed alignment with input bar

--- src/components/ExchangeRate/ExchangeRate.tsx
+++ src/components/ExchangeRate/ExchangeRate.tsx
@@ -8,12 +8,12 @@ interface ExchangeRateProps {
 
 export function ExchangeRate({ value, name }: ExchangeRateProps) {
   return (
-    <div key={name} className='flex flex-row w-full my-4 px-5'>
-      <span className='block text-uphold-text font-semibold text-2xl truncate'>
+    <div key={name} className='flex flex-row w-full py-4 px-5'>
+      <span className='text-uphold-text font-semibold text-2xl truncate pl-1'>
         {formatCurrency(limitDecimalPlaces(value))}
       </span>
       <span className='flex-grow' />
-      <Currency className='mr-9' currency={name} />
+      <Currency className='mr-10' currency={name} />
     </div>
   );
 }
