6405456ee5bc9d030a55d1f436ccfdd45b0e1c10
Pedro Morim <1180798@isep.ipp.pt>
2022-01-08 20:21:33 +0000

    revert: :fire: removed poorly implemented debouncing

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,3 @@
-import { useMemo, useState } from 'react';
-import lodash from 'lodash';
-
 import { parseInput } from '../../utils';
 
 interface InputProps {
@@ -9,28 +6,12 @@ interface InputProps {
 }
 
 export function Input({ value, setValue }: InputProps) {
-  const [input, setInput] = useState(value);
-
-  const debouncedParse = useMemo(
-    () => lodash.debounce((input) => parseInput(input, setValue), 500),
-    // Make sure to only create the debounced function once
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  );
-
-  function onChange({
-    target: { value: newValue },
-  }: React.ChangeEvent<HTMLInputElement>): void {
-    setInput(newValue);
-    debouncedParse(newValue);
-  }
-
   return (
     <input
       className='bg-uphold-paper text-uphold-text text-5xl rounded-lg h-20 w-full pl-5 pr-40 form-input focus:bg-white border-0 focus:border-2 focus:border-uphold-text focus:drop-shadow-none placeholder-uphold-muted'
       type='text'
-      value={input}
-      onChange={onChange}
+      value={value}
+      onChange={(e) => parseInput(e.currentTarget.value, setValue)}
       placeholder='0.00'
       aria-labelledby='currency amount'
     />
