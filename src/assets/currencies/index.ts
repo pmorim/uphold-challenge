f5c53dbf58b31fc961edc080beaaa78a9b51b76c
Pedro Morim <1180798@isep.ipp.pt>
2022-01-09 22:11:45 +0000

    fix: :bug: fixed images on production mode

--- src/assets/currencies/index.ts
+++ src/assets/currencies/index.ts
@@ -1,47 +1,83 @@
-/**
- * Unfortunately, I had to create this Set manually.
- * That is because the assets didn't cover all of the currencies of the Ticker.
- * Therefore, to my knowledge, there is no API call that could generate it.
- */
+import AED from './AED.png';
+import ARS from './ARS.png';
+import AUD from './AUD.png';
+import BAT from './BAT.png';
+import BCH from './BCH.png';
+import BRL from './BRL.png';
+import BTC from './BTC.png';
+import BTG from './BTG.png';
+import CAD from './CAD.png';
+import CHF from './CHF.png';
+import CNY from './CNY.png';
+import Crypto from './Crypto.png';
+import DASH from './DASH.png';
+import DKK from './DKK.png';
+import ETH from './ETH.png';
+import EUR from './EUR.png';
+import GBP from './GBP.png';
+import HKD from './HKD.png';
+import ILS from './ILS.png';
+import INR from './INR.png';
+import JPY from './JPY.png';
+import KES from './KES.png';
+import LTC from './LTC.png';
+import MXN from './MXN.png';
+import NOK from './NOK.png';
+import NZD from './NZD.png';
+import PHP from './PHP.png';
+import PLN from './PLN.png';
+import RVR from './RVR.png';
+import SEK from './SEK.png';
+import SGD from './SGD.png';
+import UAE from './UAE.png';
+import USD from './USD.png';
+import VOX from './VOX.png';
+import XAG from './XAG.png';
+import XAU from './XAU.png';
+import XPD from './XPD.png';
+import XPT from './XPT.png';
+import XRP from './XRP.png';
 
-export const supportedCurrencies = new Set<string>([
-  'AED',
-  'ARS',
-  'AUD',
-  'BAT',
-  'BCH',
-  'BRL',
-  'BTC',
-  'BTG',
-  'CAD',
-  'CHF',
-  'CNY',
-  'Crypto',
-  'DASH',
-  'DKK',
-  'ETH',
-  'EUR',
-  'GBP',
-  'HKD',
-  'ILS',
-  'INR',
-  'JPY',
-  'KES',
-  'LTC',
-  'MXN',
-  'NOK',
-  'NZD',
-  'PHP',
-  'PLN',
-  'RVR',
-  'SEK',
-  'SGD',
-  'UAE',
-  'USD',
-  'VOX',
-  'XAG',
-  'XAU',
-  'XPD',
-  'XPT',
-  'XRP',
-]);
+export const iconMap: Record<string, string> = Object.freeze({
+  AED: AED,
+  ARS: ARS,
+  AUD: AUD,
+  BAT: BAT,
+  BCH: BCH,
+  BRL: BRL,
+  BTC: BTC,
+  BTG: BTG,
+  CAD: CAD,
+  CHF: CHF,
+  CNY: CNY,
+  Crypto: Crypto,
+  DASH: DASH,
+  DKK: DKK,
+  ETH: ETH,
+  EUR: EUR,
+  GBP: GBP,
+  HKD: HKD,
+  ILS: ILS,
+  INR: INR,
+  JPY: JPY,
+  KES: KES,
+  LTC: LTC,
+  MXN: MXN,
+  NOK: NOK,
+  NZD: NZD,
+  PHP: PHP,
+  PLN: PLN,
+  RVR: RVR,
+  SEK: SEK,
+  SGD: SGD,
+  UAE: UAE,
+  USD: USD,
+  VOX: VOX,
+  XAG: XAG,
+  XAU: XAU,
+  XPD: XPD,
+  XPT: XPT,
+  XRP: XRP,
+});
+
+export const supportedCurrencies = new Set<string>(Object.keys(iconMap));
--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,3 +1,5 @@
+import { iconMap } from '../../assets/currencies';
+
 interface CurrencyProps {
   currency: string;
   className?: string;
@@ -9,11 +11,7 @@ export function Currency({ currency, className }: CurrencyProps) {
       className={`flex flex-row justify-start items-center space-x-2 w-16
       ${className ?? ''}`}
     >
-      <img
-        className='w-6'
-        src={`src/assets/currencies/${currency}.png`}
-        alt={currency}
-      />
+      <img className='w-6' src={iconMap[currency]} alt={currency} />
       <span className='block text-uphold-text font-semibold mr-10'>
         {currency}
       </span>
