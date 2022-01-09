640adafab89fc16cf0b32a4882ae13ae8a57c5aa
Pedro Morim <1180798@isep.ipp.pt>
2022-01-09 18:30:05 +0000

    refactor: :recycle: improved tailwind classes structure

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -34,7 +34,7 @@ export function Input({ value, setValue }: InputProps) {
 
   return (
     <input
-      className='bg-uphold-paper text-uphold-text text-5xl rounded-lg h-20 w-full pl-5 pr-40 form-input focus:bg-white border-0 focus:border-2 focus:border-uphold-text focus:drop-shadow-none placeholder-uphold-muted'
+      className='form-input h-20 w-full pl-5 pr-40 text-5xl rounded-lg bg-uphold-paper text-uphold-text placeholder-uphold-muted focus:bg-white border-0 focus:border-uphold-text focus:border-2  focus:drop-shadow-none'
       type='text'
       placeholder='0.00'
       aria-labelledby='currency amount'
