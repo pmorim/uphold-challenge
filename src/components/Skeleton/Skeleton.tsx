353ebc5ee85648dab9eeaf548a3aae630d87f0a7
Pedro Morim <1180798@isep.ipp.pt>
2022-01-10 00:37:32 +0000

    fix: :bug: fixed bug where Skeleton wouldn't generate a width

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,9 +1,7 @@
 export function Skeleton() {
-  const width = Math.round(Math.random() * 3 + 7);
-
   return (
     <div className='animate-pulse flex flex-row items-center w-full py-4 px-5 pr-16'>
-      <div className={`rounded-full bg-gray-300 h-6 w-[${width}rem]`} />
+      <div className='rounded-full bg-gray-300 h-6 w-32' />
       <span className='flex-grow' />
       <div className='rounded-full bg-gray-300 h-6 w-6 mr-1' />
       <div className='rounded-full bg-gray-300 h-4 w-8' />
